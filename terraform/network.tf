e70540ca276c8093a853fe5d208157e1b44092e0
sofia <me@Sofia-MacBook-Pro.local>
2024-10-26 19:44:16 +0900

    feat: terraform 바뀐코드 추가-로컬설치용

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.service_name}-vpc"
+    "kubernetes.io/cluster/${var.service_name}" = "shared"
   }
 }
 
@@ -11,9 +14,12 @@ resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.service_name}-subnet-public-a"
+    "kubernetes.io/cluster/${var.service_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
 
@@ -22,9 +28,12 @@ resource "aws_subnet" "public_c" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-northeast-2c"
-
+  map_public_ip_on_launch = true
+  
   tags = {
     Name = "${var.service_name}-subnet-public-c"
+    "kubernetes.io/cluster/${var.service_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
 
@@ -36,6 +45,8 @@ resource "aws_subnet" "private_a" {
 
   tags = {
     Name = "${var.service_name}-subnet-private-a"
+    "kubernetes.io/cluster/${var.service_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
 
@@ -47,6 +58,8 @@ resource "aws_subnet" "private_c" {
 
   tags = {
     Name = "${var.service_name}-subnet-private-c"
+    "kubernetes.io/cluster/${var.service_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
 
@@ -84,7 +97,6 @@ resource "aws_route_table_association" "public_c" {
   subnet_id      = aws_subnet.public_c.id
   route_table_id = aws_route_table.public.id
 }
-
 
 ## output ##
 
