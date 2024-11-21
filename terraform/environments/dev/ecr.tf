164c8ef4d6f4a03215cf553ec98b470fe2e06023
Kim Seung-yeop <a01055143647@gmail.com>
2024-11-21 14:11:38 +0900

    add: jenkins용 ECR 추가

--- terraform/environments/dev/ecr.tf
+++ terraform/environments/dev/ecr.tf
@@ -1,12 +1,25 @@
 locals {
-  ecr_name_prefix = "${local.service_name}-${local.environment}"
+  ecr_name_prefix = "${local.service_name}-${local.environment}"  # 공통 Prefix 설정
 }
 
+# ArgoCD용 ECR
 module "argocd_ecr" {
-  source           = "../../modules/ecr"           # 모듈 경로
-  repository_name  = "${local.ecr_name_prefix}-ecr-argocd"          # 원하는 리포지토리 이름으로 변경
+  source           = "../../modules/ecr"               # 기존 ArgoCD ECR 모듈 경로
+  repository_name  = "${local.ecr_name_prefix}-ecr-argocd"  # 기존 ArgoCD ECR 이름
 }
 
+# Jenkins용 ECR 추가
+module "jenkins_ecr" {
+  source           = "../../modules/ecr"               # 동일한 모듈 재사용
+  repository_name  = "${local.ecr_name_prefix}-ecr-jenkins"  # Jenkins용 ECR 이름
+}
+
+# 기존 ArgoCD ECR URL 출력
 output "argocd_ecr_url" {
   value = module.argocd_ecr.ecr_repository_url
+}
+
+# Jenkins ECR URL 출력 추가
+output "jenkins_ecr_url" {
+  value = module.jenkins_ecr.ecr_repository_url
 }
